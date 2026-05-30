a9630a7ab7cfcf7ec6829c8818a08527a59a22f8
bubunyo nyavor <bubunyo@upvest.co>
2026-05-30 14:01:45 +0200

    improve some elements

--- src/content.config.ts
+++ src/content.config.ts
@@ -35,7 +35,7 @@ const partners = defineCollection({
     name: z.string().min(1),
     logo: z.string().url(),
     url: z.string().url().optional(),
-    backgroundColor: z.string().regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/).optional(),
+    backgroundColor: z.string().regex(/^#([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/).optional(),
   }),
 });
 
